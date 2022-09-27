5b825187380c85791d76cabb371c35d61c84e713
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-09-27 13:40:29 -0600

    don e

--- script.js
+++ script.js
@@ -1,130 +1,132 @@
 function greeting(name, timeOfDay) {
-    return `Hello ${name}. Have a great ${timeOfDay}`
+  return `Hello ${name}. Have a great ${timeOfDay}`;
 }
 
-console.log(greeting("will", "morning"))
+console.log(greeting("will", "morning"));
 
 function fahrToCels(fahrTemp) {
-    return (fahrTemp - 32) * 5 / 9;
+  return ((fahrTemp - 32) * 5) / 9;
 }
 
-console.log(fahrToCels(32))
+console.log(fahrToCels(32));
 
 function heronsFormula(a, b, c) {
-    s = (a + b + c) / 2
-    return Math.sqrt(s * (s - a) * (s - b) * (s - c))
+  s = (a + b + c) / 2;
+  return Math.sqrt(s * (s - a) * (s - b) * (s - c));
 }
 
-console.log(heronsFormula(3, 4, 5))
+console.log(heronsFormula(3, 4, 5));
 
 function heightCheck(height) {
-    if (height >= 82) {
-        return "All Aboard!";
-    } else {
-        return "Too short for ride."
-    }
+  if (height >= 82) {
+    return "All Aboard!";
+  } else {
+    return "Too short for ride.";
+  }
 }
 
-console.log(heightCheck(100))
+console.log(heightCheck(100));
 
 function analyzeNumber(nAnalyze) {
-    if (nAnalyze > 0) {
-        return "positive";
-    } else if (nAnalyze < 0) {
-        return "negative";
-    } else if (nAnalyze === 0) {
-        return "zero"
-    }
+  if (nAnalyze > 0) {
+    return "positive";
+  } else if (nAnalyze < 0) {
+    return "negative";
+  } else if (nAnalyze === 0) {
+    return "zero";
+  }
 }
 
-console.log(analyzeNumber(-2))
+console.log(analyzeNumber(-2));
 
 function gradeFeedback(grade) {
-    if (grade >= 80) {
-        return "Good job";
-    } else if (grade >= 50) {
-        return "passable";
-    } else if (grade < 50) {
-        return "Keep trying";
-    }
+  if (grade >= 80) {
+    return "Good job";
+  } else if (grade >= 50) {
+    return "passable";
+  } else if (grade < 50) {
+    return "Keep trying";
+  }
 }
 
-console.log(gradeFeedback(80))
+console.log(gradeFeedback(80));
 
 function abs(nAbs) {
-    if (nAbs > 0) {
-        return nAbs;
-    } else if (nAbs < 0) {
-        return nAbs + (nAbs * -2)
-    } else if (nAbs === 0) {
-        return 0;
-    }
+  if (nAbs > 0) {
+    return nAbs;
+  } else if (nAbs < 0) {
+    return nAbs + nAbs * -2;
+  } else if (nAbs === 0) {
+    return 0;
+  }
 }
 
-console.log(abs(-45692345990))
+console.log(abs(-45692345990));
 
 function isEven(nIsEven) {
-    if (nIsEven % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
+  if (nIsEven % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(isEven(-5))
+console.log(isEven(-5));
 
 function isDivisibleBy(nDivisible, divisor) {
-    if (nDivisible % divisor === 0) {
-        return true;
-    } else {
-        return false;
-    }
+  if (nDivisible % divisor === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(isDivisibleBy(-18, 4))
+console.log(isDivisibleBy(-18, 4));
 
 function pointLocation(x, y) {
-    if (x === 0 && y === 0) {
-        return "origin";
-    } else if (y === 0) {
-        return "x-axis";
-    } else if (x === 0) {
-        return "y-axis";
-    } else if (x > 0 && y > 0) {
-        return "quadrant I";
-    } else if (x < 0 && y > 0) {
-        return "quadrant II";
-    } else if (x < 0 && y < 0) {
-        return "quadrant III";
-    } else if (x > 0 && y < 0) {
-        return "quadrant IV";
-    } 
+  if (x === 0 && y === 0) {
+    return "origin";
+  } else if (y === 0) {
+    return "x-axis";
+  } else if (x === 0) {
+    return "y-axis";
+  } else if (x > 0 && y > 0) {
+    return "quadrant I";
+  } else if (x < 0 && y > 0) {
+    return "quadrant II";
+  } else if (x < 0 && y < 0) {
+    return "quadrant III";
+  } else if (x > 0 && y < 0) {
+    return "quadrant IV";
+  }
 }
 
-console.log(pointLocation(1, -1))
+console.log(pointLocation(1, -1));
 
 function ordinalAdjective(nOrdinal) {
-    if (nOrdinal === 1) {
-        return "1st";
-    } else if (nOrdinal === 2) {
-        return "2nd";
-    } else if (nOrdinal === 3) {
-        return "3rd";
-    } else {
-        return nOrdinal + "th";
-    }
+  if (nOrdinal === 1) {
+    return "1st";
+  } else if (nOrdinal === 2) {
+    return "2nd";
+  } else if (nOrdinal === 3) {
+    return "3rd";
+  } else {
+    return nOrdinal + "th";
+  }
 }
 
-console.log(ordinalAdjective(9))
+console.log(ordinalAdjective(9));
 
 function minPayment(balance) {
+  if (balance > 10) {
     if ((balance * 2.1) / 100 > 10) {
-        return (balance * 2.1) / 100;
-    } else if ((balance * 2.1) / 100 < 10){
-        return 10;
-    } else if (balance < 10) {
-        return balance;
+      return (balance * 2.1) / 100;
+    } else if ((balance * 2.1) / 100 < 10) {
+      return 10;
     }
+  } else {
+    return balance;
+  }
 }
 
-console.log(minPayment(9))
+console.log(minPayment(7));
